c5bae700ce91f2df0cd5c1bbc8a5c4f10eed082d
HoornbloemCoding <71282824+HoornbloemCoding@users.noreply.github.com>
2021-07-26 18:01:59 +0200

    change files

--- script.js
+++ script.js
@@ -1,33 +1,34 @@
-const cirkel = document.querySelector(".cirkel")
-const btn = document.querySelector("button")
-const mijnBody = document.querySelector(".nieuweCirkels")
+myDots = document.querySelectorAll(".dot");
+apple = document.querySelector(".apple");
+place = document.querySelector(".extraDots");
 
-// console.log(cirkel)
+let colors = ["blue", "green", "yellow", "orange", "red", "purple", ]
+amountofDots = 1;
 
-cirkel.style.backgroundColor = "red"
+apple.addEventListener("mouseover", createDot);
+document.addEventListener("click", createDot);
 
-// cirkel.addEventListener("click", function(){
-//     cirkel.style.backgroundColor = "green"
-// })
+document.addEventListener("mousemove", function(e){
+    myleft = e.pageX + "px";
+    mytop = e.pageY + "px";
 
-// cirkel.addEventListener("mouseover", function(e){
-//     console.log(e)
-//     // cirkel.style.backgroundColor = "yellow"
-// })
-
-function maakCirkel(){
-    const mijnCirkel = document.createElement("div");
-    mijnCirkel.classList.add("cirkel")
-    console.log(mijnCirkel)
-    mijnBody.append(mijnCirkel)
-}
-
-btn.addEventListener("click", maakCirkel)
-
-document.addEventListener("mousemove", function(event){
-    // console.log(event.pageX, event.pageY)
-
-    cirkel.style.transform = "translate("+event.pageX+"px ,"+event.pageY+"px)"
-    console.log("translate("+event.pageX+","+event.pageY+")")
+    myDots.forEach(dot => {
+        speed = dot.getAttribute("data-cursor");
+        dot.style.transform = "translate("+myleft+"," +mytop+")";
+        dot.style.transition = "all " + speed
+    })
 })
 
+function createDot(){
+    myNewDot = document.createElement("div");
+    myNewDot.classList.add("dot");
+    myNewDot.style.background = colors[amountofDots%6]
+
+    myNewDot.setAttribute("data-cursor",amountofDots / 20 + "s");
+    place.append(myNewDot);
+
+    myDots = document.querySelectorAll(".dot");
+
+    amountofDots += 1;
+    console.log(myNewDot.getAttribute("data-cursor"))
+}
